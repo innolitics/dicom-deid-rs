cb1105ed8764abb480c5d4aaac18fbf0a4289fc7
Reece Stevens <reecestevens24@gmail.com>
2026-02-19 12:05:30 -0600

    Allow specifying recipe vars on command line

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,46 @@ use std::env;
 use std::path::PathBuf;
 use std::process;
 
+fn print_usage(program: &str) {
+    eprintln!(
+        "Usage: {} <input_dir> <output_dir> <recipe_file> [--var NAME VALUE]...",
+        program
+    );
+    eprintln!();
+    eprintln!("Options:");
+    eprintln!("  --var NAME VALUE   Define a recipe variable (can be repeated)");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 4 {
-        eprintln!("Usage: {} <input_dir> <output_dir> <recipe_file>", args[0]);
+        print_usage(&args[0]);
         process::exit(1);
+    }
+
+    let mut variables: HashMap<String, String> = HashMap::new();
+    let mut i = 4;
+    while i < args.len() {
+        if args[i] == "--var" {
+            if i + 2 >= args.len() {
+                eprintln!("Error: --var requires NAME and VALUE arguments");
+                print_usage(&args[0]);
+                process::exit(1);
+            }
+            variables.insert(args[i + 1].clone(), args[i + 2].clone());
+            i += 3;
+        } else {
+            eprintln!("Error: unknown argument '{}'", args[i]);
+            print_usage(&args[0]);
+            process::exit(1);
+        }
     }
 
     let config = DeidConfig {
         input_dir: PathBuf::from(&args[1]),
         output_dir: PathBuf::from(&args[2]),
         recipe_path: PathBuf::from(&args[3]),
-        variables: HashMap::new(),
+        variables,
         functions: HashMap::new(),
     };
 
